bb7ba9e7fcfd5735872e5f972a4ff869944ac69f
Naufal Fikriansyah <naufalfikriansyah99@gmail.com>
2021-05-26 19:11:11 +0700

    Capstone Project Bangkit 2021

---
+++ app/src/main/java/com/example/capstoneproject/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.capstoneproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val imgButtonDesainku : ImageButton = findViewById(R.id.desainku)
+        imgButtonDesainku.setOnClickListener(this)
+
+        val imgButtonKonveksi : ImageButton = findViewById(R.id.konveksi_terdekat)
+        imgButtonKonveksi.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?){
+        when(v?.id){
+            R.id.desainku -> {
+                val I = Intent(this@MainActivity, DesainkuActivity :: class.java )
+                startActivity(I)
+            }
+            R.id.konveksi_terdekat ->{
+                val I = Intent(this@MainActivity, KonveksiActivity :: class.java )
+                startActivity(I)
+            }
+        }
+    }
+}
